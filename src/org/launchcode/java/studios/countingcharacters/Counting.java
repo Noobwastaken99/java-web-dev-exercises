cecbf4fb8212c54bc17756958175142be186c160
Noobwastaken99 <125329223+Noobwastaken99@users.noreply.github.com>
2023-03-30 19:42:37 -0500

    SuperBonus Finished

--- src/org/launchcode/java/studios/countingcharacters/Counting.java
+++ src/org/launchcode/java/studios/countingcharacters/Counting.java
@@ -1,5 +1,8 @@
 package org.launchcode.java.studios.countingcharacters;
+import java.io.IOException;
 import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Counting {
     public static void main(String[] args) {
@@ -11,20 +14,32 @@ public class Counting {
                 "be zero to start with. So if you move all the terms over to one side, you can put the quadratics into " +
                 "a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, " +
                 "it’s pretty straightforward from there.";
+        Path fileName = Path.of("/Users/estherwingate/Desktop/GitHub/Java-practice/java-web-dev-exercises/text.rtf");
 
-        System.out.println("Would you like to enter your own string yes or no?");
+        System.out.println("Would you like to enter your own string? yes or no");
         String maybe = input.nextLine();
-        System.out.println(maybe);
         if (Objects.equals(maybe, "yes")) {
             System.out.println("Enter your string:");
             quote = input.nextLine();
+        } else {
+            System.out.println("Would you like to file? yes or no");
+            maybe = input.nextLine();
+            if (Objects.equals(maybe, "yes")) {
+                System.out.println("Enter your string:");
+                try {
+                    quote = Files.readString(fileName);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
+
         char[] charactersInString = filter(quote).toLowerCase().toCharArray();
         Arrays.sort(charactersInString);
 
         for (char c : charactersInString) {
             int spot = chr.indexOf(c);
-            int count = 0;
+            int count;
             if (!chr.contains(c)) {
                 chr.add(c);
                 num.add(1);
